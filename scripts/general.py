300a22f3da91ca35a728ff4a4d50413628f68e6b
maTh51 <matheustigon@gmail.com>
2024-12-16 19:51:00 +0000

    Created general config script

--- scripts/general.py
+++ scripts/general.py
@@ -39,15 +39,44 @@ if __name__ == '__main__':
 
     results = list()
 
-    grid_size_lst = [1000]
-    steps = 1
 
+    # ---------------- Variable parameters ----------------------
+
+    grid_size_lst = [1000]
+    data_volume = 10
+    steps = 1
+    temporal_granularity = ['1D']
+
+    start_date = '2023-10-01'
+    end_date = '2024-03-01'
+    train_end_date = '2024-01-31'
+    test_end_date = '2024-02-29'
+
+    config['database']['filters']['start_date'] = start_date
+    config['database']['filters']['end_date'] = end_date
+    config['evaluation']['train_end_date'] = train_end_date
+    config['evaluation']['test_end_date'] = test_end_date
+
+    train_start_date = ['2024-06-01','2024-04-01','2024-01-01','2023-07-01','2022-07-01']
+    train_days_lst = [30, 90, 180, 360, 720]
+    test_lst = list(zip(train_start_date, train_days_lst))
+    
+    config['models']['STKDEModel']['slide_window'] = True
+
+
+    # -----------------------------------------------------------------
 
     for _ in range(steps):
 
         tmp = pd.DataFrame()
 
         for grid_size in grid_size_lst:
+
+            # Variables that need to iterate over loop
+            config['evaluation']['grid_size'] = grid_size
+            config['evaluation']['temporal_granularity'] = temporal_granularity
+            train_dt, train_days = test_lst[0]
+            config['database']['filters']['start_date'] = train_dt
 
             # Load config file
             with open(path_config) as f:
@@ -71,12 +100,13 @@ if __name__ == '__main__':
 
             print(f'Train with {grid_size} grid size...')
 
-            config['evaluation']['grid_size'] = grid_size
 
             #Create Grid
             print('Create Grid...')
             grid = create_grid(grid_size=config['evaluation']['grid_size'], 
                             municipalities=config['database']['filters']['nome_municipio'])
+
+            train_points = train_points.sample(int(train_points.shape[0]*data_volume/10))
 
             #Define start status
             time_start = time.time()
@@ -91,9 +121,22 @@ if __name__ == '__main__':
                 print(f'Train {m}...')
                 if params and 'slide_window' in params.keys():
                     del params['slide_window']
-                    model = globals()[m](points=points, grid = grid, last_train_date = config['evaluation']['train_end_date'], temporal_granularity=config['evaluation']['temporal_granularity'], **params)
+                    model = globals()[m](
+                        points=points, 
+                        grid = grid, 
+                        last_train_date = config['evaluation']['train_end_date'], 
+                        temporal_granularity=config['evaluation']['temporal_granularity'], 
+                        data_volume=data_volume/10, **params,
+                        **params
+                    )
                 else:
-                    model = globals()[m](points=train_points, grid = grid, last_train_date = config['evaluation']['train_end_date'],temporal_granularity=config['evaluation']['temporal_granularity'], **params)
+                    model = globals()[m](
+                        points=train_points, 
+                        grid = grid, 
+                        last_train_date = config['evaluation']['train_end_date'],
+                        temporal_granularity=config['evaluation']['temporal_granularity'], 
+                        **params
+                    )
                 model.train()
                 models.append(model)
             # %%
@@ -108,6 +151,8 @@ if __name__ == '__main__':
             res = eval.simulate(hit_rate_percentage=config['evaluation']['hit_rate_percentage'])
 
             res['grid_size'] = grid_size
+            res['data_volume'] = data_volume
+            res['temporal_window'] = train_dt
 
             #Define end status
             time_end = time.time()
@@ -124,7 +169,7 @@ if __name__ == '__main__':
 
             tmp = pd.concat([tmp, res])
         
-        tmp = tmp.groupby(['grid_size','model']).mean().reset_index()
+        tmp = tmp.groupby(['grid_size', 'data_volume', 'temporal_granularity', 'temporal_window', 'model']).mean().reset_index()
         results.append(tmp)
 
     csv = pd.DataFrame()
